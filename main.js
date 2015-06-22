57fab17a19f5426a89698b974d5dad74183338da
Hongzhang Luo <lhz1983001@gmail.com>
2015-06-22 22:25:03 +0800

    try filter

--- main.js
+++ main.js
@@ -6,4 +6,8 @@ app.controller("RootController", ["$scope", function($scope){
     $scope.$watch("valueCNY", function(val){
         $scope.valueUSD = val * 6 || "";
     })
+}]).filter("toMoney", [function(){
+    return function(val){
+        return parseInt(val).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+    }
 }]);
