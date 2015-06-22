464c8aab81e1e8c4e8c95c83d04bc0301823f326
Hongzhang Luo <lhz1983001@gmail.com>
2015-06-22 22:13:27 +0800

    try ng-change

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ var app = angular.module("my", []);
 
 app.controller("RootController", ["$scope", function($scope){
     $scope.title = "Hello World";
+
+    $scope.inputChanged = function(){
+        console.log("value changed");
+         $scope.valueUSD = $scope.valueCNY * 6;
+    }
 }])
