73bbc0ec9b07d45bf257200067517f5f753e6295
Hongzhang Luo <lhz1983001@gmail.com>
2015-06-23 10:10:06 +0800

    add directive

--- main.js
+++ main.js
@@ -10,4 +10,20 @@ app.controller("RootController", ["$scope", function($scope){
     return function(val){
         return parseInt(val).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
     }
+}]).directive("number", [function(){
+    return {
+        restrict: 'A',
+        require: '?ngModel',
+        link: function(scope, elm, attr, ctrl) {
+            if (!ctrl) return;
+
+
+            ctrl.$$parserName = 'isNumber';
+            ctrl.$validators.isNumber = function(modelValue, viewValue) {
+                var newVar = ctrl.$isEmpty(viewValue) ||  /^\d+$/.test(viewValue);
+                console.log(newVar);
+                return !!(newVar);
+            };
+        }
+    };
 }]);
